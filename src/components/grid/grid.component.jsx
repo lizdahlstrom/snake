43ff19a81dec5bd5b24f140f4f58169e2284f741
Liz Dahlström <liz.dahlstrom@gmail.com>
2021-04-13 14:12:48 +0200

    Add grid and cells

--- src/components/grid/grid.component.jsx
+++ src/components/grid/grid.component.jsx
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import GridCell from '../cell/gridcell.component';
 import Snake from '../snake/snake.component';
+import { GridBox } from './grid.styles';
+
+const SIZE = 16;
+
+const generateCells = () => {
+  const generatedCells = [];
+
+  for (let i = 0; i < SIZE; i++) {
+    for (let j = 0; j < SIZE; j++) {
+      generatedCells.push({ id: `${i}${j}` });
+    }
+  }
+
+  console.log('wihoo', generatedCells);
+
+  return generatedCells;
+};
 
 const Grid = () => {
+  // Create a grid with x amount of cells
+  // React can keep track of which cells are populated by the snake
+  // When the cell is populated it should show a different color than the other cells
+  // this can be achieved with css, using a class for the populated cells
+  const [cells, setCells] = useState(generateCells());
+
   return (
-    <div>
-      Grid
-      <Snake />
-    </div>
+    <GridBox size={SIZE}>
+      {cells.length > 0 ? cells.map((cell) => <GridCell id={cell.id} />) : 'Hmmm...'}
+    </GridBox>
   );
 };
 
